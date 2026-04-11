e7266bc7e10851a2e49693fe336580dae5eb7ffd
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-04-11 12:49:37 +0800

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](auto& x, auto& y) {
+            return x.start < y.start;
+        });
+        for(int i = 1; i < n; i++) {
+            int x1 = intervals[i - 1].end;
+            int x2 = intervals[i].start;
+            if(x1 > x2)
+                return false;
+        }
+
+        return true;
+    }
+};

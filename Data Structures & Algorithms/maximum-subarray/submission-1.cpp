c20ad3b7c9c15c87544d092306deb1ff9f426159
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-04-22 20:50:02 +0800

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // reason : if cur sum becomes negative, keeping it will reduce the sum of any future ans 
+    // method : if cur sum becomes negative then reset it
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0];
+        int cur = 0;
+        for(int num : nums) {
+            if(cur < 0)
+                cur = 0;
+            cur += num;
+            res = max(res, cur);
+        }
+
+        return res;
+    }
+};

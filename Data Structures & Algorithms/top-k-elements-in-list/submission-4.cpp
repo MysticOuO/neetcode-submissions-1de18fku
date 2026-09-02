4704292c88bc3e67cac156b0ad1f0191fb2e83a1
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-02 11:22:28 +0800

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // using the bucket sort idea (index represent occurs times) -> O(n)
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> bucket;
+        vector<vector<int>> freq(nums.size() + 1);
+        vector<int> res;
+        for(const int& num : nums)
+            ++bucket[num];
+        
+        for(pair<const int, int>& mem : bucket) 
+            freq[mem.second].push_back(mem.first);
+        
+        for(int i = freq.size() - 1; i > 0; i--) {
+            for(int& n : freq[i]) {
+                res.push_back(n);
+                if(res.size() == k)
+                    return res;
+            }
+        }
+        
+        return res;
+
+    }
+};

28c102d428b48f1442a38b8936f06ecc33b20068
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-04 01:46:23 +0800

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool help(char c) {
+        return (c >= 'a' && c <= 'z') || 
+               (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
+    bool isPalindrome(string s) {
+        int left = 0;
+        // C++ s.size() didn't consider \0 in the string 
+        // but C consider it in the string
+        int right = s.size() - 1; 
+        while(left < right) {
+            while(left < right && !help(s[left]))
+                ++left;
+            while(left < right && !help(s[right]))
+                --right;
+            if(tolower(s[left]) == tolower(s[right])) {
+                ++left;
+                --right;
+            }
+
+            else 
+                return false;
+        }
+
+        return true;
+    }
+};

a6d8e02284fc32bb8740ff9f936f7e6055582aec
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-04 02:04:06 +0800

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // Using Binary Search to find the index
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for(int i = 0; i < numbers.size(); i++) {
+            int count = target - numbers[i];
+            // Remember that cannot be same index
+            int left = i + 1;
+            int right = numbers.size() - 1;
+            while(left <= right) {
+                int mid = left + (right - left) / 2;
+                if(numbers[mid] < count)
+                    left = mid + 1;
+                else if(numbers[mid] > count)
+                    right = mid - 1;
+                else 
+                    return {i + 1, mid + 1};
+            }
+        }
+
+        return {};
+    }
+};

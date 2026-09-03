2ffbdb20c5623f5ed680702df47ab7debc4cc9d3
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-04 01:59:20 +0800

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> table;
+        for(int i = 0; i < numbers.size(); i++)
+            table[numbers[i]] = i;
+
+        for(int i = 0; i < numbers.size(); i++) {
+            int remain = target - numbers[i];
+            if(table.find(remain) != table.end())
+                return {i + 1, table[remain] + 1};
+        }   
+
+        return {};
+    }
+};

69ca654d17a803f0b9cc7758946a0dc2b63728a9
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-05 14:19:50 +0800

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // result depend on the shrorter wall 
+    int trap(vector<int>& height) {
+        int res = 0;
+        int lwall = height[0];
+        int rwall = height[height.size() - 1];
+        int l = 0;
+        int r = height.size() - 1;
+
+        while(l < r) {
+            if(lwall < rwall) {
+                l++;
+                lwall = max(lwall, height[l]);
+                res += lwall - height[l];
+            }
+            else {
+                r--;
+                rwall = max(rwall, height[r]);
+                res += rwall - height[r];
+            }
+        }
+
+        return res;
+
+    }
+};

5c02bd9bccdcd32e620e9b8d663f48881b1637e4
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-09 20:34:51 +0800

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> table = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for(const char& c : s) {
+            if(table.count(c)) {
+                if(!st.empty() && st.top() == table[c])
+                    st.pop();
+                else 
+                    return false;
+            }
+            else 
+                st.push(c);
+        }
+
+        return st.size() == 0;
+    }
+};

e404ce2495ecf2a06e074d7305555dddb82f187d
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-15 19:16:34 +0800

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,32 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> table;
+
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        table[key].emplace_back(timestamp, value);
+    }
+    
+    string get(string key, int timestamp) {
+        // remember using reference to avoid TLE
+        // using auto values = table[key]; will copy entire vector
+        auto& values = table[key];
+        int left = 0;
+        int right = values.size() - 1;
+        string res = "";
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(values[mid].first <= timestamp) {
+                res = values[mid].second;
+                left = mid + 1;
+            }
+
+            else 
+                right = mid - 1;
+        }
+
+        return res;
+    }
+};

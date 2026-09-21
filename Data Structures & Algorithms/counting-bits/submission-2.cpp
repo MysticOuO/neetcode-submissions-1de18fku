21f8fceaa1cec3a9ccbe0e92ae37acc2c44ce12a
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-21 14:05:50 +0800

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int count(int n) {
+        int res = 0;
+        while(n) {
+            n = n & (n - 1);
+            ++res;
+        }
+        return res;
+    }
+
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i = 0; i <= n; i++) 
+            ans.emplace_back(count(i));
+        return ans;
+    }
+};

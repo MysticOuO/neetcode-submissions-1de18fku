7d1c0c465d4782ff7550ada953851896dc9f1783
MysticOuO <90899913+MysticOuO@users.noreply.github.com>
2026-09-22 16:59:00 +0800

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int res = 0;
+        int pos = 31;
+
+        while(n) {
+            if(n % 2 == 1) 
+                res += pow(2, pos);
+            --pos;
+            n /= 2;
+        }
+
+        return res;
+    }
+};
